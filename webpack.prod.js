4608fbc3a380fd88a40ad1bb91b1a200f9eb8e6a
Jeet <jeetpatel0070070@gmail.com>
2022-01-01 13:44:50 +0530

    add

---
+++ webpack.prod.js
@@ -0,0 +1,105 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const buildPath = path.resolve(__dirname, "dist");
+const BundleAnalyzePlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = {
+  // This option controls if and how source maps are generated
+  devtool: "source-map",
+  entry: {
+    index: "./src/javascript/index.js",
+    case: "./src/javascript/case.js",
+    about: "./src/javascript/about.js",
+    contact: "./src/javascript/contact.js",
+  },
+  output: {
+    filename: "[name].[hash:20].js",
+    path: buildPath,
+  },
+  plugins: [
+    new BundleAnalyzePlugin(),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      inject: "body",
+      chunks: ["index"],
+      filename: "index.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/case.html",
+      inject: "body",
+      chunks: ["case"],
+      filename: "case.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/about.html",
+      inject: "body",
+      chunks: ["about"],
+      filename: "about.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/contact.html",
+      inject: "body",
+      chunks: ["contact"],
+      filename: "contact.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+      chunkFilename: "[id].[contenthash].css",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env"],
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(glsl|vs|fs|vert|frag)$/,
+        exclude: /node_modules/,
+        use: ["raw-loader", "glslify-loader"],
+      },
+    ],
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      name: false,
+    },
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          sourceMap: true,
+        },
+        parallel: true,
+      }),
+      new OptimizeCssAssetsPlugin({}),
+    ],
+  },
+};
